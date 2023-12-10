ebc5902d017d06d2e7fc5d0410ac1e83f1b71d04
Kalyan Mallajosyula <mallajosyulak@calaces.org>
2023-12-11 00:41:20 +0530

    Initial Commit

---
+++ src/main/java/com/uthinkurbetterthanme/helloenv/App.java
@@ -0,0 +1,18 @@
+package com.uthinkurbetterthanme.helloenv;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class App {
+    public static void main(String[] args) {
+        try {
+            String environmentFile = System.getProperty("environment.file", "dev.properties");
+            InputStream is = App.class.getClassLoader().getResourceAsStream(environmentFile);
+            Properties props = new Properties();
+            props.load(is);
+
+            System.out.println("Hello from " + props.getProperty("environment"));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
